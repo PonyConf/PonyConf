dab6bf83f9eae113497df6ad2e40cce65ed6543b
Élie Bouttier <elie@bouttier.eu>
2016-06-12 18:47:26 +0200

    auth related minor changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 from .views import profile
 
 urlpatterns = [
     url(r'^profile$', profile, name='profile'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'', include('django.contrib.auth.urls')),
 ]
