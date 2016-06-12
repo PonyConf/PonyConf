de72b6b167853ebf863ea6d6a5d5f693439f6ae1
Élie Bouttier <elie@bouttier.eu>
2016-06-12 20:02:59 +0200

    fix settings utilisation

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 
 from .views import profile
-from ponyconf.settings import LOGOUT_REDIRECT_URL
+from django.conf import settings
 
 urlpatterns = [
     url(r'^profile$', profile, name='profile'),
-    url(r'^logout/$', auth_views.logout, {'next_page': LOGOUT_REDIRECT_URL}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
     url(r'', include('django.contrib.auth.urls')),
 ]
