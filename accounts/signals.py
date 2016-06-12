7476994dc0d1922ec429f79257d5a31ced88ccb4
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-06-12 21:31:11 +0200

    flake8

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 def on_user_logged_in(sender, request, **kwargs):
     messages.success(request, 'Welcome!')
 
+
 @receiver(user_logged_out)
 def on_user_logged_out(sender, request, **kwargs):
     messages.success(request, 'Goodbye!')
