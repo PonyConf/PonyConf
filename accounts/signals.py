c5ac3411b1109083f00949e627c69fdf2dd6f493
Élie Bouttier <elie@bouttier.eu>
2016-06-12 19:50:11 +0200

    welcome / goodbye messages

---
+++ accounts/signals.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+from django.contrib import messages
+
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, request, **kwargs):
+    messages.success(request, 'Welcome!')
+
+@receiver(user_logged_out)
+def on_user_logged_out(sender, request, **kwargs):
+    messages.success(request, 'Goodbye!')
