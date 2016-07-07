7f8fa7864b3f1aacdbc538bad34f50e4e9d5f6a2
Élie Bouttier <elie@bouttier.eu>
2016-07-08 01:12:18 +0200

    fix orga_required & staff_required decorators

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -1,15 +1,20 @@
 from functools import wraps
+from django.core.exceptions import PermissionDenied
 
 from accounts.utils import is_orga, is_staff
 
 
 def orga_required(func):
     def _is_orga(request, *args, **kwargs):
-        return is_orga(request, request.user)
+        if is_orga(request, request.user):
+            return view_func(request, *args, **kwargs)
+        raise PermissionDenied
     return wraps(func)(_is_orga)
 
 
-def staff_required(func):
+def staff_required(view_func):
     def _is_staff(request, *args, **kwargs):
-        return is_staff(request, request.user)
-    return wraps(func)(_is_staff)
+        if is_staff(request, request.user):
+            return view_func(request, *args, **kwargs)
+        raise PermissionDenied
+    return wraps(view_func)(_is_staff)
