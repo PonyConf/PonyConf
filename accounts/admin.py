c3a96ececfbf9cea9982f0e98a7240e2f551d744
Élie Bouttier <elie@bouttier.eu>
2016-07-22 17:53:51 +0200

    add Transport & Connector to django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from accounts.models import Participation, Profile
+from accounts.models import Participation, Profile, Transport, Connector
 
 admin.site.register(Profile)  # FIXME extend user admin
 admin.site.register(Participation)
+admin.site.register(Transport)
+admin.site.register(Connector)
