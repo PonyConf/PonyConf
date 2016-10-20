933d468dcbaa3a2235a1b78e1d1fbd76c67de721
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-10-20 15:14:37 +0200

    fix RoomAdmin

--- planning/admin.py
+++ planning/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 from planning.models import Room
+from ponyconf.admin import SiteAdminMixin
 
 
-class RoomAdmin(admin.ModelAdmin):
-    fields = ('name', 'label', 'capacity', 'site')
+class RoomAdmin(SiteAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'label', 'capacity')
 
 
 admin.site.register(Room, RoomAdmin)
