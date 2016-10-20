7a021b89061b07e6c4ce17cbe306d7381729d060
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-10-20 14:34:26 +0200

    more details in Room's django admin

--- planning/admin.py
+++ planning/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from planning.models import Room
 
 
-admin.site.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    fields = ('name', 'label', 'capacity', 'site')
+
+
+admin.site.register(Room, RoomAdmin)
