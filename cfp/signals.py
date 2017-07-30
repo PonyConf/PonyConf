8fb064ac8cd95f3fccead9c881d20a59f2f48add
Élie Bouttier <elie@bouttier.eu>
2017-07-30 18:01:41 +0200

    fix: cfp app config was not loaded

--- cfp/signals.py
+++ cfp/signals.py
@@ -3,15 +3,17 @@ from django.dispatch import receiver
 from django.contrib.sites.models import Site
 from django.conf import settings
 
+from ponyconf.decorators import disable_for_loaddata
 from .models import Conference
 
 
 @receiver(post_save, sender=Site, dispatch_uid="Create Conference for Site")
 @disable_for_loaddata
 def create_conference(sender, instance, **kwargs):
-    Conference.objects.get_or_create(site=instance)
+    conference, created = Conference.objects.get_or_create(site=instance)
 
 
+# connected in apps.py
 def call_first_site_post_save(apps, **kwargs):
     try:
         site = Site.objects.get(id=getattr(settings, 'SITE_ID', 1))
