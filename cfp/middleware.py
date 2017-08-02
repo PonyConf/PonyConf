d14d04e7c79d557b2fdc33b6f62af2552c8214bb
Élie Bouttier <elie@bouttier.eu>
2017-08-02 20:50:16 +0200

    migrating to new middleware format

--- cfp/middleware.py
+++ cfp/middleware.py
@@ -1,9 +1,13 @@
-from django.core.exceptions import ImproperlyConfigured
-
 from .utils import get_current_conf
 
 
 class ConferenceMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
     def process_view(self, request, view, view_args, view_kwargs):
         if view.__module__ != 'cfp.views':
             return
