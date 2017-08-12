623b94fccdf3b6555c2c885bd678b756c2256aed
Élie Bouttier <elie@bouttier.eu>
2017-08-12 13:54:48 +0200

    removing old accounts app

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from accounts.models import Participation, Profile, Transport, Connector
-
-
-admin.site.register(Profile)  # FIXME extend user admin
-admin.site.register(Participation)
-admin.site.register(Transport)
-admin.site.register(Connector)
--- accounts/mixins.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-
-from .utils import is_staff
-
-
-class StaffRequiredMixin(UserPassesTestMixin):
-    def test_func(self):
-        return is_staff(self.request, self.request.user)
